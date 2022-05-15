a6377a344ae7c5697605db387026c9f0eb7df871
libin <libinnzs@163.com>
2022-05-15 23:29:59 +0800

    feat: 项目初始化，vue-router、vuex集成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <router-link to="/login">登录页</router-link>
+  <router-link to="/main">主页面</router-link>
+  <h2>{{ $store.state.name }}</h2>
+  <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 })
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,34 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 方式一：直接通过CLI提供给我们的选项来配置：
+  outputDir: './build',
+
+  // 方式二：通过configureWebpack修改webpack的配置：
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       views: '@/views'
+  //     }
+  //   }
+  // }
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, './src'),
+  //     components: '@/components',
+  //     common: '@/common',
+  //     utils: '@/utils',
+  //     api: '@/api',
+  //     views: '@/views'
+  //   }
+  // }
+
+  // 方式三：通过chainWebpack修改webpack的配置：
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('views', '@/views')
+  }
 })
